48e6ef3e32a2b34c7e8a6abc6b5f1ec4caa0391b
tankedshoe <tankedshoe@gmail.com>
2018-03-28 08:41:44 -0600

    Suped up the art panel.

--- src/art/view/ArtPanel.java
+++ src/art/view/ArtPanel.java
@@ -1,6 +1,19 @@
 package art.view;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.Ellipse2D;
+import java.util.Hashtable;
+import javax.swing.*;
+import javax.swing.event.*;
+import art.controller.ArtController;
+
 public class ArtPanel extends JPanel
 {
-
+	private final int MINIMUM_EDGE = 5;
+	private final int MAXIMUM_EDGE = 20;
+	private final int MINIMUM_SCALE = 20;
+	private final int MAXIMUM_SCALE = 100;
+	
+	private ArtController app;
 }
